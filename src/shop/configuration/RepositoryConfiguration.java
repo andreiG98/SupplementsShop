3a63f680cd4919e9899688f324c6130e9bf83a45
Andrei Gidea <andrei.gadea@yahoo.com>
2019-05-04 21:57:16 +0300

    csv files

--- src/shop/configuration/RepositoryConfiguration.java
+++ src/shop/configuration/RepositoryConfiguration.java
@@ -3,13 +3,14 @@ package shop.configuration;
 import shop.domain.repository.*;
 
 public class RepositoryConfiguration {
-    private final CustomerRepository customerRepository = new CustomerRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\customer-file.csv");
-    private final InvoiceRepository invoiceRepository = new InvoiceRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\invoice-file.csv");
-    private final CourierRepository courierRepository = new CourierRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\courier-file.csv");
-    private final OrderRepository orderRepository = new OrderRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\order-file.csv");
-    private final ProteinRepository proteinRepository = new ProteinRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\product-file.csv");
-    private final VitaminRepository vitaminRepository = new VitaminRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\product-file.csv");
-    private final ProducerRepository producerRepository = new ProducerRepository("D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\producer-file.csv");
+    private final String path = "D:\\JavaProjects\\SupplementsShop\\src\\shop\\configuration\\";
+    private final CustomerRepository customerRepository = new CustomerRepository(path + "customer-file.csv");
+    private final InvoiceRepository invoiceRepository = new InvoiceRepository(path + "invoice-file.csv");
+    private final CourierRepository courierRepository = new CourierRepository(path + "courier-file.csv");
+    private final OrderRepository orderRepository = new OrderRepository(path + "order-file.csv");
+    private final ProteinRepository proteinRepository = new ProteinRepository(path + "product-file.csv");
+    private final VitaminRepository vitaminRepository = new VitaminRepository(path + "product-file.csv");
+    private final ProducerRepository producerRepository = new ProducerRepository(path + "producer-file.csv");
 
     private static RepositoryConfiguration ourInstance = new RepositoryConfiguration();
 
